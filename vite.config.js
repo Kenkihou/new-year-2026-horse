60b4c306df69093c4113609b93b33c87f817be51
Kenkihou <45kaku34kaku@gmail.com>
2025-12-30 00:21:32 +0900

    fix base path to relative

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/new-year-2026-horse/', // ←ここを追加！前後にスラッシュが必要です
+  base: './',
 })
